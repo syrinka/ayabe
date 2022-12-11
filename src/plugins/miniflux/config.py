f885c41629169d21a3e5e74f3b6960632bb57236
syrinka <syrinka@foxmail.com>
2022-12-11 13:32:23 +0800

    miniflux config

--- src/plugins/miniflux/config.py
+++ src/plugins/miniflux/config.py
@@ -1,9 +1,12 @@
 from typing import List, Optional
-from pydantic import BaseSettings
+from pydantic import BaseSettings, Extra
 
 
-class Config(BaseSettings):
+class Config(BaseSettings, extra=Extra.ignore):
     miniflux_api_endpoint: str
     miniflux_api_token: str
+    miniflux_check_cron: str = '0 * * * *'
+    miniflux_crop_length: int = 10
     miniflux_trace_feeds: Optional[List[int]] = None
     miniflux_trace_categories: Optional[List[int]] = None
+    superusers: List[str]
